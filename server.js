3ba0450c9831d5f4d8ab812f939d6ca4e6d72fea
Kurivyan <arman.azamatov.2015@mail.ru>
2022-06-07 19:54:56 +0600

    1

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({extended : true}))
 app.use(cookieParser({secret : '123'}))
 
 app.get('/', (req, res) => {
-    res.write('Hello World')
+    res.send('Hello World')
 })
 
-app.listen( 3000, () => {console.log(`Server Started`)})
+app.listen(process.env.PORT || 3000, () => {console.log(`Server Started`)})
