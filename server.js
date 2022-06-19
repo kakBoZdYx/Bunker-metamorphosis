261b92ef456bd5bd8673010b63232c278c99c004
Kurivyan <arman.azamatov.2015@mail.ru>
2022-06-19 20:41:35 +0600

    Registration
    Adding Registration Form

--- server.js
+++ server.js
@@ -2,37 +2,46 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
-const { MongoClient } = require('mongodb')
+const mongodb = require('mongodb')
 const MongoStore = require('connect-mongo')
 const path = require('path')
 const session = require('express-session')
 
 app.use(bodyParser.urlencoded({extended : true}))
-app.use(cookieParser({secret : '123'}))
+app.use(cookieParser())
 app.use(express.static(path.join(__dirname, '/src/')))
+app.use(session({ 
+        secret : ['key', 'secretSid', 'seed'],
+        resave : true,
+        cookie: { 
+                httpOnly: true,
+                maxAge: null 
+        },
+        store : MongoStore.create({ mongoUrl: 'mongodb+srv://kurivyan:Qqwerty123@cluster0.hflo0.mongodb.net/?retryWrites=true&w=majority' })
+}))
 
-//routs
+app.set('view engine', 'ejs')//view engine
+app.set('views', path.join(__dirname + "/src/views"))
+
+//MongoDb Connection
+const uri = "mongodb+srv://kurivyan:Qqwerty123@cluster0.hflo0.mongodb.net/?retryWrites=true&w=majority"; 
+const mongoClient = new mongodb.MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: mongodb.ServerApiVersion.v1 });
+
+
+//user_rout
 const user = require(path.join(__dirname, '/src/routs/user.js'))
 app.use('/user', user)
 
-app.use( //cookie connect
-    session({
-        secret : 'secretkey',
-        key : 'seed',
-        cookie : {
-            httpOnly : true,
-            maxAge : null
-        },
-        store: MongoStore.create({mongoUrl : 'mongodb+srv://kurivyan:Qwerty123@cluster0.hflo0.mongodb.net/?'})
-    })
-)
+
+app.get('/test', (req, res) => {
+        console.log(req.session)
+        res.redirect('/')
+})
 
 app.get('/', (req, res) => {
-    if(req.session.auth && req.session.auth == true) {
-        res.render('index')
-    } else if (req.session.auth == false || !req.session.auth) {
-        res.render('index', {sessionUser})
-    }
+        sessionData = req.session
+        pageName = 'home'
+        res.render('index', {sessionData, pageName})
 })
 
 app.listen(process.env.PORT || 3000, () => {console.log(`Server Started`)})
--- src/routs/user.js
+++ src/routs/user.js
@@ -1,8 +1,47 @@
 const { Router } = require('express')
+const mongodb = require('mongodb')
 const router = Router()
 
-router.get('/profile', (req, res) => {
+const uri = "mongodb+srv://kurivyan:Qqwerty123@cluster0.hflo0.mongodb.net/?retryWrites=true&w=majority"; 
+const mongoClient = new mongodb.MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: mongodb.ServerApiVersion.v1 });
 
+router.get('/profile', (req, res) => {
+    sessionData = req.session
+    pageName = 'Profile'
+    res.render('profile', {sessionData, pageName})
+})
+
+router.get('/login', (req, res) => {
+
+    sessionData = req.session
+    pageName = 'login'
+    
+})
+
+router.post('/registration', (req, res) => {
+
+    var reg_login = req.body.reglogin
+    var reg_password = req.body.regpassword
+
+    var registrationSchema = {
+        'login' : reg_login,
+        'password' : reg_password
+    } 
+
+    mongoClient.connect(async function(error, mongo) {
+        let db = mongo.db('mainDataSet');
+        let coll = db.collection('users');
+               
+        await coll.insertOne(registrationSchema);
+        res.redirect('/')
+        })
+})
+.get('/registration', (req, res) => {
+
+    sessionData = req.session
+    pageName = 'registration'
+
+    res.render('registration', {pageName})
 })
 
 module.exports = router
